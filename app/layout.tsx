878fe7fb8586954bbd9ee2c34df97b5f61a1c2a6
legisquestoesconcurso-ui <legisquestoesconcurso@gmail.com>
2026-04-17 13:59:28 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,13 @@ import type {Metadata} from 'next';
 import './globals.css'; // Global styles
 
 export const metadata: Metadata = {
-  title: 'SER+ App',
+  title: 'SER+',
   description: 'Aplicativo de leitura e desenvolvimento pessoal',
+  icons: {
+    icon: '/icon.png',
+    apple: '/icon.png',
+  },
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
